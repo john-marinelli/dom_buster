27ad956ab1489c7210e9850f7f92285b0f2c9160
John M <john@pop-os.localdomain>
2022-04-14 12:48:27 -0600

    integrated twill to handle form listing and input

--- input_driver.py
+++ input_driver.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
-from bs4 import BeautifulSoup
 
 #TODO filter stdout to reduce visual clutter and useless error messages
 #this seems to work, but gets rid of the ability to ctrl+c to quit the session
@@ -13,57 +12,13 @@ from bs4 import BeautifulSoup
 class InputDriver:
     
     driver = None
-    home_url = None
-    current_url = None
-    html_source = None
-    test_input = None
-    #attributes = []
-    input_fields = ""
 
     def __init__(self, url, test_input):
+        pass
+    
+    def test_url(self, url):
         self.driver = webdriver.Chrome()
         self.driver.get(url)
-        self.home_url = self.driver.current_url
-        self.current_url = self.home_url
-        self.input_fields = self.driver.find_elements_by_tag_name('input')
-        self.test_input = test_input
-    
-    def input_fuzz_attr(self):
-
-        for box in self.input_fields:
-            box.send_keys(self.test_input)
-            box.submit()
-            WebDriverWait(self.driver, 15).until(EC.url_changes(self.current_url))
-            self.current_url = self.driver.current_url
-            #TODO check for dom changes, url stuff for XSS
-
-            html_source = self.driver.page_source
-            soup = BeautifulSoup(html_source, 'html.parser')
-            i = 0
-            for node in soup.find_all():
-                if any(self.test_input in i for i in node.attrs.values()):
-                    print("Input found in an html element's attributes")
-                    i += 1
-            #print("Input was found in " + i "html element's attributes.")
-            self.driver.get(self.home_url)
-            self.current_url = self.home_url
-    
-    def input_fuzz_text(self):
-        #TODO figure out how to check for input escaping
-        for box in self.input_fields:
-            box.send_keys(self.test_input)
-            box.submit()
-            WebDriverWait(self.driver, 15).until(EC.url_changes(self.current_url))
-            self.current_url = self.driver.current_url
-            #TODO check for dom changes, url stuff for XSS
-
-            html_source = self.driver.page_source
-            soup = BeautifulSoup(html_source, 'html.parser')
-            i = 0
-            for node in soup.find_all():
-                if self.test_input in node.text:
-                    print("Input found in an html element's text.")
-                    i += 1
-            #print("Input was found in " + i "html element's attributes.")
-            self.driver.get(self.home_url)
-            self.current_url = self.home_url
+        if input("Press q to quit: \n") == "q":
+            return True
+        
--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 from input_driver import InputDriver as ID
+from twill_ui import FormGrabber
 
 def main():
-    url = "https://ac5a1f621f1519fdc080ac95001f00bd.web-security-academy.net/"
-    test_input = "jghgjghg"
-    driver = ID(url, test_input)
+    url = "https://acdf1fab1f30670ac0ee982300ee006f.web-security-academy.net/"
+    form_name = ""
+    test_input = "\"><script>alert(1)</script>"
+    form_grabber = FormGrabber(url)
+    input_tester = ID(url, test_input)
+    
 
-    driver.input_fuzz_attr()
+
+    form_grabber.load_page()
+    form_grabber.show_forms()
+    print(form_grabber.get_current_url())
+    form_num = input("Select form number: \n")
+    form_name = input("Select form name (leave blank if none): \n")
+    button = input("Select button to press: \n")
+    form_grabber.input_forms(form_num, form_name, test_input, button)
+    print(form_grabber.get_current_url())
+
+    input_tester.test_url(str(form_grabber.get_current_url()))
 
 
 

64bc5f0476a1487ae4fe0f0d7cdc9e0302dd0877
John M <john@pop-os.localdomain>
2022-04-21 12:29:49 -0600

    fixed crashing when alert pops up

--- dom_buster.py
+++ dom_buster.py
@@ -8,7 +8,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 #TODO switch over input display and selection to selenium
 #TODO incorporate URL parameters 
 def main():
-    url = "https://ac7e1fd61fe9fde3c0977f2100c10085.web-security-academy.net/"
+    url = "https://ac501f3c1fc190c5c08d0ee300490084.web-security-academy.net/"
     xpath = None
     input_tester = UrlOpener(url)
     # payloads = open_payloads()
@@ -34,26 +34,15 @@ def main():
                 curr_input = input_fields[i]
                 curr_input.send_keys(payload)
                 curr_input.submit()
-                #TODO this webdriverwait breaks the program when an alert pops up
                 try:
-                    WebDriverWait(input_tester.get_driver(), 3).until(EC.alert_is_present(), "Timed out waiting for PA creation " + "confirmation popup to appear")
+                    WebDriverWait(input_tester.get_driver(), 3).until(EC.alert_is_present(), "")
                     alert = input_tester.get_driver().switch_to.alert
                     alert.accept()
-                    print("alert accepted")
+                    print("alert detected")
                 except:
-                    print("no alert")
-                # if input_tester.alert_check():
-                #     print("alert present")
+                    print("no alert detected")
                 input_tester.load_url(url)
                 input_fields = input_tester.list_inputs()
-
-    #TODO tell if alert is present https://stackoverflow.com/questions/21729464/how-to-check-if-an-alert-is-present-and-if-yes-then-accept-it
-
-    #TODO iterate over inputs
-    
-    #TODO add check for displayed and interactable 
-    #https://stackoverflow.com/questions/30826671/how-to-check-if-an-element-is-clickable-or-not-using-selenium-webdriver
-
 
 if __name__ == "__main__":
     main()
